465be96efc03066e9f43c1fc2e5e94565aea6949
slippy404 <mikeedey2403@gmail.com>
2026-08-02 02:36:57 +0100

    Add files via upload

---
+++ Models/Song.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPlayer.Models;
+public class Song
+{
+    //used to play the file
+    public string FilePath { get; set; } = string.Empty;
+    //metadata read from ID3 tags
+    public string Title { get; set; } = "Unknown Title";
+    public string Artist { get; set; } = "Unknown Artist";
+    public string Album { get; set; } = "Unknown Album";
+    //displays how much of the song is left
+    public TimeSpan Duration { get; set; }
+    //cover art
+    public string? AlbumArtUrl { get; set; }
+}
